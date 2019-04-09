a983354adb4944338c78f6c884e6e5dd93a6e51e
ravedave <dic15dju@student.lu.se>
2019-04-09 09:33:05 +0200

    Initial commit including all files

---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+// Code from
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(3000);
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }
+    }
+}
+
+
