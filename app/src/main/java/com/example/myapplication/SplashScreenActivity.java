057ffa9c9ee3ad3353431377bc4777a4c78a66e3
ravedave <dic15dju@student.lu.se>
2019-04-09 11:44:45 +0200

    Initial commit including all files

--- app/src/main/java/com/example/myapplication/SplashScreenActivity.java
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -1,18 +1,18 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-
-// Code from
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
