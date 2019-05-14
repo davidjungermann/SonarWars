1266f81f4daeb9214c4ebaec7629c8522624197d
ravedave <dic15dju@student.lu.se>
2019-05-14 14:49:04 +0200

    Delay with randomly generated enemies

--- app/src/main/java/com/example/myapplication/EnemyLayer.java
+++ app/src/main/java/com/example/myapplication/EnemyLayer.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.timer.ITimerCallback;
+import org.andengine.engine.handler.timer.TimerHandler;
 import org.andengine.entity.Entity;
 import org.andengine.entity.modifier.LoopEntityModifier;
 import org.andengine.entity.modifier.MoveModifier;
@@ -18,6 +20,8 @@ public class EnemyLayer extends Entity {
     public static EnemyLayer instance;
     public int enemyCount;
     public Camera mCamera;
+    public TimerHandler time;
+    public int width;
 
     public static EnemyLayer getSharedInstance() {
         return instance;
@@ -45,19 +49,25 @@ public class EnemyLayer extends Entity {
         clearEntityModifiers();
         clearUpdateHandlers();
         mCamera = MainActivity.getSharedInstance().mCamera;
-        int width = (int) mCamera.getWidth();
-        int height = (int) mCamera.getHeight();
-        Random rand = new Random(width);
 
         for(int i = 0; i < enemyCount; i++){
-            Enemy e = (Enemy) EnemyPool.sharedEnemyPool().obtainPoolItem();
-            e.sprite.setPosition(mCamera.getCenterX() + rand.nextFloat(), -20);
-            e.sprite.setVisible(true);
-            attachChild(e.sprite);
-            enemies.add(e);
-            setVisible(true);
-            MoveYModifier moveDown = new MoveYModifier(10, mCamera.getHeight(), -20);
-            registerEntityModifier(new LoopEntityModifier(moveDown));
+            registerUpdateHandler(time = new TimerHandler(1, new ITimerCallback() {
+
+                @Override
+                public void onTimePassed(TimerHandler pTimerHandler) {
+                    Random rand = new Random();
+                    width = (int) mCamera.getWidth();
+                    Enemy e = (Enemy) EnemyPool.sharedEnemyPool().obtainPoolItem();
+                    e.sprite.setPosition(rand.nextInt(width), -20);
+                    e.sprite.setVisible(true);
+                    attachChild(e.sprite);
+                    enemies.add(e);
+                    setVisible(true);
+                    MoveYModifier moveDown = new MoveYModifier(10, mCamera.getHeight(), -20);
+                    e.sprite.registerEntityModifier(new LoopEntityModifier(moveDown));
+                }
+            }));
+
         }
     }
     public void purge() {
