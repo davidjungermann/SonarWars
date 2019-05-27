1b26f2fa049ef72c870917ed95edd5e9bfb344eb
oskarstrom7 <36918573+oskarstrom7@users.noreply.github.com>
2019-05-27 19:02:24 +0200

    Highscore now working

---
+++ app/src/main/java/com/example/myapplication/Score.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+
+public class Score implements Comparable<Score> {
+    private String scoreDate;
+    public int scoreNum;
+
+    public Score(String date, int num){
+        scoreDate=date;
+        scoreNum=num;
+    }
+
+    @Override
+    public int compareTo(Score sc) {
+        //return 0 if equal
+        //1 if passed greater than this
+        //-1 if this greater than passed
+        return sc.scoreNum>scoreNum? 1 : sc.scoreNum<scoreNum? -1 : 0;
+    }
+
+    public String getScoreText() {
+        return scoreDate+" - "+scoreNum;
+    }
+}
